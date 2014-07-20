b921b818739a025ac370ed7f04809452cf8a4bea
Riku Rouvila <riku.rouvila@gmail.com>
2014-07-21 02:05:02 +0300

    add auth plugin

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var irc = require('slate-irc');
 var net = require('net');
+var mongoose = require('mongoose');
 
 var env = process.env.NODE_ENV || 'development';
 var production = env === 'production';
@@ -10,8 +11,11 @@ var config = {
   realName: 'botti the bot',
   host: production ? 'irc.quakenet.org' : 'localhost',
   port: 6667,
-  channels: production ? ['#h4x3d'] : ['#test']
+  channels: production ? ['#h4x3d'] : ['#test'],
+  database: 'ircbot'
 }
+
+mongoose.connect('mongodb://localhost/' + config.database);
 
 var stream = net.connect({
   host: config.host,
@@ -26,5 +30,6 @@ client.join(config.channels);
 
 client.use(require('./lib/plugins/logger'));
 client.use(require('./lib/plugins/commands'));
+client.use(require('./lib/plugins/auth'));
 client.use(require('./lib/plugins/title'));
 client.use(require('./lib/plugins/spotify'));
