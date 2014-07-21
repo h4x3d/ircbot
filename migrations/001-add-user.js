cced287d9bd66088375bf0c5aae920e0ecd35c37
Riku Rouvila <riku.rouvila@gmail.com>
2014-07-21 03:00:44 +0300

    add migrations and first migration to create admin user

---
+++ migrations/001-add-user.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+var User = require('../lib/models/user');
+
+mongoose.connect('mongodb://localhost/ircbot'); // TODO replace with config.database
+
+exports.up = function(next){
+  User.find({}).exec()
+  .then(function(users) {
+    if(users.length > 0) {
+      return;
+    }
+    return User.create({ // TODO replace with config.admin
+      username: 'admin',
+      password: 'foobar'
+    });
+
+  }, next)
+  .then(next.bind(null, null), next)
+};
+
+exports.down = function(next){
+  next();
+};
