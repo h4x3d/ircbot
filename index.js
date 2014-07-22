af83a6b33bb636893cd6d183d850fdd8df7fa20d
Riku Rouvila <riku.rouvila@gmail.com>
2014-07-22 17:23:07 +0300

    store config to .config.<env>.json files

--- index.js
+++ index.js
@@ -6,51 +6,39 @@ var net = require('net');
 var mongoose = require('mongoose');
 
 var config = require('./lib/utils/config');
-var Config = require('./lib/models/config');
 
 var logger = require('./lib/plugins/logger');
 
 mongoose.connect('mongodb://' + config.database.host + '/' + config.database.name);
 
-Config.get().then(function(config) {
+var stream = net.connect({
+  host: config.host,
+  port: config.port,
+});
 
-  if(!config) {
-    throw new Error('Configuration not found. Check README.md for installation instructions');
+var client = irc(stream);
+
+client.nick(config.nickname);
+client.user(config.username, config.realName);
+
+client.use(logger);
+client.use(require('./lib/plugins/channels'));
+client.use(require('./lib/plugins/say'));
+client.use(require('./lib/plugins/auth'));
+client.use(require('./lib/plugins/title'));
+client.use(require('./lib/plugins/spotify'));
+client.use(require('./lib/plugins/h'));
+client.use(require('./lib/plugins/laugh'));
+
+var nicknameTries = 0;
+
+client.on('welcome', function() {
+  client.join(config.channels);
+});
+
+client.on('error', function(message) {
+  if(message.command === 'ERR_NICKNAMEINUSE') {
+    nicknameTries++;
+    client.nick(config.nickname + nicknameTries);
   }
-
-  var stream = net.connect({
-    host: config.host,
-    port: config.port,
-  });
-
-  var client = irc(stream);
-
-  client.nick(config.nickname);
-  client.user(config.username, config.realName);
-
-  client.use(logger);
-  client.use(require('./lib/plugins/channels'));
-  client.use(require('./lib/plugins/say'));
-  client.use(require('./lib/plugins/auth'));
-  client.use(require('./lib/plugins/title'));
-  client.use(require('./lib/plugins/spotify'));
-  client.use(require('./lib/plugins/h'));
-  client.use(require('./lib/plugins/laugh'));
-
-  var nicknameTries = 0;
-
-  client.on('welcome', function() {
-    client.join(config.channels);
-  });
-
-  client.on('error', function(message) {
-    if(message.command === 'ERR_NICKNAMEINUSE') {
-      nicknameTries++;
-      client.nick(config.nickname + nicknameTries);
-    }
-  });
-
-}).then(function() {}, logger.error);
-
-
-
+});
