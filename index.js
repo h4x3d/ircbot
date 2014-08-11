a1a46a33153ec20bc32237e3b5317385ba710643
Riku Rouvila <riku.rouvila@gmail.com>
2014-08-11 23:36:01 +0300

    implement command argument parser, add color plugin

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ client.use(require('./lib/plugins/say'));
 client.use(require('./lib/plugins/auth'));
 client.use(require('./lib/plugins/title'));
 client.use(require('./lib/plugins/spotify'));
+client.use(require('./lib/plugins/colors'));
 // client.use(require('./lib/plugins/h'));
 // client.use(require('./lib/plugins/laugh'));
 // client.use(require('./lib/plugins/api'));
