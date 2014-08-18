4961a75afab441718b1b049ba07d580e299627e7
Riku Rouvila <riku.rouvila@gmail.com>
2014-08-18 20:49:34 +0300

    warn users about links with extension not matching the real content-type

--- test/plugins/title.js
+++ test/plugins/title.js
@@ -2,6 +2,9 @@
 
 var assert = require('assert');
 var util = require('../util');
+var mime = require('mime');
+var url = require('url');
+var http = require('http');
 
 describe('"title" plugin', function(){
 
@@ -19,6 +22,22 @@ describe('"title" plugin', function(){
       assert.equal(message.from, this.client.me);
       assert.equal(message.message, 'Twitter');
       done();
+    }.bind(this));
+  });
+
+  it('should tell if url\'s extension doesnt match the real content type', function(done) {
+
+    var server = http.createServer( function(request, response) {
+      var pathname = url.parse(request.url).pathname;
+      response.writeHead(200, {'Content-Type': mime.lookup('text/html; charset=utf-8')});
+    });
+
+    server.listen(9005);
+
+    this.spy.send(this.client.me, 'http://localhost:9005/1kez9.png');
+    this.spy.on('message', function(message) {
+      assert.equal(message.message, 'Content type "text/html; charset=utf-8" doesn\'t match the extension ".png"');
+      server.close(done);
     }.bind(this));
   });
 
