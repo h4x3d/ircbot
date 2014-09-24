1430052c00ec0eacb7f15c875da37cf461a00713
Riku Rouvila <riku.rouvila@gmail.com>
2014-09-24 19:36:46 +0300

    reconnect after connection is lost

--- index.js
+++ index.js
@@ -6,32 +6,54 @@ var net = require('net');
 var mongoose = require('mongoose');
 
 var config = require('./lib/config');
-
 var logger = require('./lib/utils/logger');
+
+var client = null;
 
 mongoose.connect('mongodb://' + config.database.host + '/' + config.database.name);
 
-var stream = net.connect({
-  host: config.host,
-  port: config.port,
-});
+function connect() {
 
-var client = irc(stream);
+  logger.log('Connecting to ' + config.host);
 
-client.nick(config.nickname);
-client.user(config.username, config.realName);
+  var stream = net.connect({
+    host: config.host,
+    port: config.port,
+  });
 
-client.use(require('./lib/plugins/logger'));
-client.use(require('./lib/plugins/channels'));
-client.use(require('./lib/plugins/say'));
-client.use(require('./lib/plugins/auth'));
-client.use(require('./lib/plugins/title'));
-client.use(require('./lib/plugins/spotify'));
-client.use(require('./lib/plugins/colors'));
-// client.use(require('./lib/plugins/h'));
-// client.use(require('./lib/plugins/laugh'));
-// client.use(require('./lib/plugins/api'));
-client.use(require('./lib/plugins/nick'));
-client.use(require('./lib/plugins/math'));
+  client = null;
+  client = irc(stream);
 
-client.stream.on('error', logger.error);
+  configure();
+}
+
+function configure() {
+  client.nick(config.nickname);
+  client.user(config.username, config.realName);
+
+  client.use(require('./lib/plugins/logger'));
+  client.use(require('./lib/plugins/channels'));
+  client.use(require('./lib/plugins/say'));
+  client.use(require('./lib/plugins/auth'));
+  client.use(require('./lib/plugins/title'));
+  client.use(require('./lib/plugins/spotify'));
+  client.use(require('./lib/plugins/colors'));
+  // client.use(require('./lib/plugins/h'));
+  // client.use(require('./lib/plugins/laugh'));
+  // client.use(require('./lib/plugins/api'));
+  client.use(require('./lib/plugins/nick'));
+  client.use(require('./lib/plugins/math'));
+
+  client.stream.on('error', function(err) {
+    logger.error(err);
+
+    if(err.code === 'ECONNREFUSED') {
+      setTimeout(connect, 5000);
+    }
+
+  });
+
+  client.stream.on('end', connect);
+}
+
+connect();
